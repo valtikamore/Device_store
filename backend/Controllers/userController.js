d0fb4306d8ea4ea970bfd5ccf830ad97b1f15446
valtikamore <77681004+valtikamore@users.noreply.github.com>
2021-07-04 15:15:54 +0300

    controllers/errors/model/routes / create - read operations (postman)

---
+++ backend/Controllers/userController.js
@@ -0,0 +1,18 @@
+const ApiError = require('../error/apiError')
+class UserController {
+    async registration (req,res) {
+
+    }
+    async login (req,res) {
+
+    }
+    async check (req,res,next) {
+        const {id} = req.query
+        if(!id) {
+           return next(ApiError.badRequest('id is absent'))
+        }
+        res.json(id)
+    }
+}
+
+module.exports = new UserController()
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,34 @@
 require('dotenv').config()
 const express = require('express')
+const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/errorHandlingMiddleware')
+const path = require('path')
+
 const port = process.env.PORT ||  5000
 const app = express()
-app.listen(port,() => console.log(`server starts at port ${port}`))
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname,'static')))
+app.use(fileUpload({}))
+app.use('/api',router)
+
+// error handling . last middleware
+app.use(errorHandler)
+
+
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(port,() => console.log(`server starts at port ${port}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
