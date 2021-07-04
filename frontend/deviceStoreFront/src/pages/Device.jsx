a97ed9d0555c9cf676434c8d6cc6867d18bcc61a
valtikamore <77681004+valtikamore@users.noreply.github.com>
2021-07-05 00:37:50 +0300

    mob x / routes

---
+++ frontend/deviceStoreFront/src/pages/Device.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DevicePage = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default DevicePage;
+
---
+++ frontend/deviceStoreFront/src/pages/shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            auth
+        </div>
+    );
+};
+
+export default Shop;
